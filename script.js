d985d13734035ad6d72eec6844149edbc7c2b852
Subham Saha <subhamranjansaha006@gmail.com>
2026-04-15 19:40:43 +0530

    Created proper HTML structure and created questions and function to display each question and check the answer

---
+++ script.js
@@ -0,0 +1,77 @@
+let timeInterval=null;
+let timeLeft=30;
+let score=0;
+let Qindex=0;
+
+
+
+let questions = [{
+    question : "Who holds the record for the most centuries in International Cricket",
+    options : ["Virat Kohli","Ricky Pointing","Sachin Tendulkar","Rohit Sharma"],
+    correct : "Sachin Tendulkar"
+},
+{
+    question : "Who holds the record for the fastest century in ODIs?",
+    options : ["AB de Villiers","Virat Kohli","Rohit Sharma","Chris Gayle"],
+    correct : "AB de Villiers"
+},
+{
+    question : "Which bowler has taken the most wickets in Test cricket?",
+    options : ["James Anderson","Shane Warne","Muttiah Muralitharan","Anil Kumble"],
+    correct : "Muttiah Muralitharan"
+},
+{
+    question : "Who is the only player to score a century and take a five-wicket haul in the same match on Test debut?",
+    options: ["Kapil Dev","Ian Botham","Bruce Taylor","Jacques Kallis"],
+    correct : "Bruce Taylor"
+},
+{
+    question : "Which cricketer was involved in the famous “Mankading” dismissal of Jos Buttler in IPL 2019?",
+    options: ["Ravichandran Ashwin","Ravindra Jadeja","Sunil Narine","Yuzvendra Chahal"],
+    correct : "Ravichandran Ashwin"
+},
+{
+    question : "Who was the first bowler to take a hat-trick in T20 Internationals?",
+    options: ["Jasprit Bumrah","Brett Lee","Lasith Malinga","Dale Steyn"],
+    correct : "Brett Lee"
+},
+{
+    question : "Who is the only Indian cricketer to have played 100+ international matches in all three formats?",
+    options: ["Rohit Sharma","MS Dhoni","Sachin Tendulkar","Virat Kohli"],
+    correct : "Virat Kohli"
+},
+{
+    question : "Which ground hosted the first-ever day-night Test match?",
+    options: ["Eden Gardens","Adelaide Oval","Dubai International Stadium","Lord's Cricket Ground"],
+    correct : "Adelaide Oval"
+},
+{
+    question : "Who was the first cricketer to hit 6 sixes in an over in international cricket?",
+    options: ["Kieron Pollard","Yuvraj Singh","Herschelle Gibbs","Tim David"],
+    correct : "Herschelle Gibbs"
+},
+{
+    question : "Which of the given below is NOT a nickname given to The Legendary MS Dhoni by his fans",
+    options: ["Thala","Dhobi","Captain Cool","DRS(Dhoni Review System)"],
+    correct : "Dhobi"
+}];
+function loadQuestion()
+{
+    let ques=document.getElementById("Question");
+    ques.textContent=questions[Qindex].question;
+    let opt=document.getElementById("options");
+    opt.innerHTML="";
+    questions[Qindex].options.forEach(function(option){
+        let btn=document.createElement("button");
+        btn.textContent=option;
+        opt.appendChild(btn);
+        btn.onclick= function() {checkAnswer(btn.textContent)};
+    })
+}
+function checkAnswer(answer)
+{
+    if(answer===questions[Qindex].correct)
+        score++;
+    document.getElementById("next-btn").style.display="block";
+}
+loadQuestion();
